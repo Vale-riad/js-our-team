d0b6bfe3f1f9cb814dcf7b4671d7345f2eb7ff8d
Vale-riad <valentinariad91@gmail.com>
2022-11-07 18:16:10 +0100

    modifiche

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 //Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 //Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 
+
 //MILESTONE 0:
 //Creare l’array di oggetti con le informazioni fornite.
 const profile = [

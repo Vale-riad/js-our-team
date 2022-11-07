f74469cff6fce7568de196eb6bcac45edfb70fee
Vale-riad <valentinariad91@gmail.com>
2022-11-07 15:03:33 +0100

    Aggiunge stampa nel dom

--- js/main.js
+++ js/main.js
@@ -42,6 +42,12 @@ const profile = [
 //Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 for (let i = 0; i < profile.length; i++) {
     console.log(profile[i].name, profile[i].role, profile[i].img);
+    const listProfiles = `<li>
+    <h4>${profile}</h4>
+    </li>
+    `;
+    document.getElementById('list-profils').innerHTML += listProfiles;
 }
 //MILESTONE 2:
 //Stampare le stesse informazioni su DOM sottoforma di stringhe
+

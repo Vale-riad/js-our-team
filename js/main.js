1c8aff27c93af6fbf7feb57d81af1e14511cabb5
Vale-riad <valentinariad91@gmail.com>
2022-11-07 14:24:22 +0100

    Corregge stampa in console

--- js/main.js
+++ js/main.js
@@ -12,36 +12,36 @@ const profile = [
         img : 'wayne-barnett-founder-ceo.jpg',
     },
     {
-        name : 'Wayne Barnett',
-        role : 'Founder & CEO',
-        img : 'wayne-barnett-founder-ceo.jpg',
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        img: 'angela-caroll-chief-editor.jpg'
     },
     {
-        name : 'Wayne Barnett',
-        role : 'Founder & CEO',
-        img : 'wayne-barnett-founder-ceo.jpg',
+        name: "Walter Gordon",
+        role: "Office Manager",
+        img: "walter-gordon-office-manager.jpg",
     },
     {
-        name : 'Wayne Barnett',
-        role : 'Founder & CEO',
-        img : 'wayne-barnett-founder-ceo.jpg',
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg",
     },
     {
-        name : 'Wayne Barnett',
-        role : 'Founder & CEO',
-        img : 'wayne-barnett-founder-ceo.jpg',
+        name: "Scott Estrada",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg",
     },
     {
-        name : 'Wayne Barnett',
-        role : 'Founder & CEO',
-        img : 'wayne-barnett-founder-ceo.jpg',
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg",
     },
 ];
 
 //MILESTONE 1:
 //Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-for(let key in profile) {
-    console.log(key, profile[key]);
+for (let i = 0; i < profile.length; i++) {
+    console.log(profile[i].name, profile[i].role, profile[i].img);
 }
 //MILESTONE 2:
 //Stampare le stesse informazioni su DOM sottoforma di stringhe

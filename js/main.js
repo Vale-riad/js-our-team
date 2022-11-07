e77dd244b5bb529946dd6964971476c76b025486
Vale-riad <valentinariad91@gmail.com>
2022-11-07 13:40:08 +0100

    Aggiunge array di oggetti

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
 'use strict'
+
+//Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+//Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+//MILESTONE 0:
+//Creare l’array di oggetti con le informazioni fornite.
+const profile = [
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        img : 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        img : 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        img : 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        img : 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        img : 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        img : 'wayne-barnett-founder-ceo.jpg',
+    },
+];
+console.log(profile);
+//MILESTONE 1:
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//MILESTONE 2:
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
